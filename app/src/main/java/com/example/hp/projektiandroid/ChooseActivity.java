e8a300c3c9ecafceabead242a8fcb0cba13f08b2
RilindLipa1 <rilindlipa@hotmail.com>
2018-06-09 13:58:08 +0200

    ChooseActivity - Update 2

--- app/src/main/java/com/example/hp/projektiandroid/ChooseActivity.java
+++ app/src/main/java/com/example/hp/projektiandroid/ChooseActivity.java
@@ -5,20 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ChooseActivity extends AppCompatActivity {
-    Spinner spinner, spinner1;
-    ArrayAdapter<CharSequence> adapter,adapter1;
+    Spinner spinner, spinner1,spinner2;
+    ArrayAdapter<CharSequence> adapter,adapter1,adapter2;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
-
+        //first spinner(drop down list)
         spinner =(Spinner) findViewById(R.id.spinner);
-        spinner1 =(Spinner) findViewById(R.id.spinner1);
+
 
         adapter = ArrayAdapter.createFromResource(this,R.array.homes,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -38,6 +42,8 @@ public class ChooseActivity extends AppCompatActivity {
 
             }
         });
+        //second spinner (drop down list)
+        spinner1 =(Spinner) findViewById(R.id.spinner1);
         adapter1 = ArrayAdapter.createFromResource(this,R.array.pyetja2,android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
@@ -53,5 +59,17 @@ public class ChooseActivity extends AppCompatActivity {
 
             }
         });
+        //third spinner(drop down list)
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        adapter2 = ArrayAdapter.createFromResource(this,R.array.guests,android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+    }
+    public void checkedButton(View v){
+        radioGroup = (RadioGroup)findViewById(R.id.radioG);
+        int radioID = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioID);
+
+        //Toast.makeText(this,"Selected radio button:"+ radioButton,Toast.LENGTH_LONG).show();
     }
 }
